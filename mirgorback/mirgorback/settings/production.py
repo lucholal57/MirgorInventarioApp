1ea1734e17dae34c6214fd98ef3212939687d34c
luciano <luciano@mirgor.com>
2022-04-16 11:29:18 -0300

    collectstatic

--- mirgorback/mirgorback/settings/production.py
+++ mirgorback/mirgorback/settings/production.py
@@ -15,7 +15,11 @@ DATABASES ={
     }
 }
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
