e8cdcdb97283193a5a0fcb73373267f39c7e5d0d
luciano <luciano@mirgor.com>
2022-04-18 10:57:13 -0300

    nombre bd

--- mirgorback/mirgorback/settings/production.py
+++ mirgorback/mirgorback/settings/production.py
@@ -8,8 +8,8 @@ DATABASES ={
     'default':{
         'ENGINE' : 'django.db.backends.postgresql_psycopg2',
         'NAME' : 'it_inventario',
-        'USER' : 'mirgor',
-        'PASSWORD' : 'mirgor',
+        'USER' : 'postgres',
+        'PASSWORD' : 'postgres',
         'HOST' : 'localhost',
        'PORT' : '5432',
     }
