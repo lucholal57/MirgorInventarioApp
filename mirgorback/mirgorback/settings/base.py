75ed06633b958ec0a30a32676b14e2ed9e4dd57a
luciano <luciano@mirgor.com>
2022-04-19 20:28:49 -0300

    cambios de posicion den middleware

--- mirgorback/mirgorback/settings/base.py
+++ mirgorback/mirgorback/settings/base.py
@@ -48,15 +48,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware' , 
+    'django.middleware.common.CommonMiddleware' ,
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware' , 
-    'django.middleware.common.CommonMiddleware' ,
+    
 ]
 
 ROOT_URLCONF = 'mirgorback.urls'
